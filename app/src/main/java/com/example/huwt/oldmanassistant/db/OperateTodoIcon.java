78c91f222948d5138744d6686fbd0e7edf9013b3
wentao <hu.wentao@outlook.com>
2019-01-20 20:02:31 +0800

    没有Message2

--- app/src/main/java/com/example/huwt/oldmanassistant/db/OperateTodoIcon.java
+++ app/src/main/java/com/example/huwt/oldmanassistant/db/OperateTodoIcon.java
@@ -3,11 +3,11 @@ package com.example.huwt.oldmanassistant.db;
 import android.media.Image;
 
 public class OperateTodoIcon {
-    public enum Icon{
-        private String iconName;
-        private Image img;
-        private lcon(String iconName){
-            this.iconName = iconName;
-        }
-    }
+//    public enum Icon{
+//        private String iconName;
+//        private Image img;
+//        private lcon(String iconName){
+//            this.iconName = iconName;
+//        }
+//    }
 }
